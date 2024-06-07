0762129a2171c082161abebf2e4ac7de3238860b
Olinka <olga.titzenthaler@gmail.com>
2024-06-07 04:43:31 +0200

    [@c.page title="XXX" /]

--- src/main/java/cz/czechitas/java2webapps/lekce7/service/DateDTO.java
+++ src/main/java/cz/czechitas/java2webapps/lekce7/service/DateDTO.java
@@ -3,7 +3,7 @@ package cz.czechitas.java2webapps.lekce7.service;
 import java.time.LocalDate;
 
 /**
- * @author Filip Jirsák
+ * @author Filip Jirsák třída pro přenos dat
  */
 public record DateDTO(String label, LocalDate date) {
 }
